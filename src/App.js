c738a9e6159c26f37a0be8bb7aaa785fc3fd5c82
louiseuna <louiseuna15@gmail.com>
2021-09-05 20:48:04 +0100

    added github repository link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
       <div className="App">
         <h1>Weather App</h1>
         <Form />
+        <p class="github">
+          <a
+            href="https://github.com/louiseuna/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open Source
+          </a>{" "}
+          code by Louise Dicks
+        </p>
       </div>
     </div>
   );
